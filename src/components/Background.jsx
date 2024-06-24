a36ed8a83ab44949feb6730dae98551b125391b6
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-06-24 15:06:23 +0000

    Fixing file error

---
+++ src/components/Background.jsx
@@ -0,0 +1,16 @@
+const Background = () => {
+    return (
+        <section className="p-20">
+            <div>
+                <h2>This is my Story...</h2>
+                <p>I am a passionate full-stack web developer who has honed my skills through formal online courses and practical project experiences. My journey began with foundational courses in HTML, CSS, and JavaScript, where I built a solid understanding of front-end development. Expanding my toolkit, I delved into frameworks like React, utilizing its power to create dynamic and responsive user interfaces.
+                    With a keen eye for design, I've integrated frameworks such as Tailwind CSS, Bootstrap, and MaterialUI into my projects, mastering the art of crafting visually appealing and functional web applications. From creating a striking Nike landing page to designing intuitive admin dashboards, each project has been an opportunity to apply my knowledge and refine my skills.
+                    Venturing into full-stack development, I developed a game discovery app that combines my front-end prowess with back-end technologies to deliver seamless user experiences. Additionally, I've built a developer portfolio that showcases my projects and growth as a developer.
+                    Looking ahead, I am eager to continue expanding my expertise, tackling new challenges, and contributing to innovative web solutions that make a positive impact.
+                </p>
+            </div>
+        </section>
+    )
+}
+
+export default Background
