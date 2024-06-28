e84e3e5f8a0226f3e37f0952190c8381af849f2d
BenedictJoachim <148780517+BenedictJoachim@users.noreply.github.com>
2024-06-28 18:07:35 +0000

    Animation: adding a on hover animation on the myProjects section cards

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,17 +7,17 @@ const Projects = () => {
         <div className="content">
             <h2 className="text-[#5800e3] text-4xl font-bold ml-5">My Projects</h2>
             <div className="flex justify-around flex-wrap">
-                <div className="w-72 m-5 bg-white p-5 rounded-lg shadow-2xl text-center">
+                <div className="w-72 m-5 bg-white p-5 rounded-lg shadow-2xl text-center transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-white border border-gray-200 rounded-lg overflow-hidden">
                     <img src={admindashboard} alt="Project 1" />
                     <a href="https://react-admin-dashboard-psi-teal.vercel.app/ecommerce" className="mt-5 text-xl">Admin Dashboard</a>
                     <p className="mt-3 text-gray-400">A MaterialUI-components web application.</p>
                 </div>
-                <div className="w-72 m-5 bg-white p-5 rounded-lg shadow-2xl text-center">
+                <div className="w-72 m-5 bg-white p-5 rounded-lg shadow-2xl text-center transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-white border border-gray-200 rounded-lg overflow-hidden">
                     <img src={gamediscoveryhub} alt="Project 2" />
                     <a href="https://game-discovery-hub-eight.vercel.app/" className="mt-5 text-xl">Game Hub</a>
                     <p className="mt-3 text-gray-400">A chackra game discovery app consuming the rawg.io API.</p>
                 </div>
-                <div className="w-72 m-5 bg-white p-5 rounded-lg shadow-2xl text-center">
+                <div className="w-72 m-5 bg-white p-5 rounded-lg shadow-2xl text-center transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-xl bg-white border border-gray-200 rounded-lg overflow-hidden">
                     <img src={nikelandingpage} alt="Project 3" />
                     <a href="https://nike-landing-page-2nyghbqiw-higheribs-projects.vercel.app/" className="mt-5 text-xl">Nike landing page</a>
                     <p className="mt-3 text-gray-400">A SPW built with tailwindcss.</p>
